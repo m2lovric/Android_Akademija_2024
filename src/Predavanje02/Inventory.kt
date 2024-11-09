94af5a7380ecca1cc05a83d44ba9d5d481515393
Luka Kordić <lkordic@hotmail.com>
2024-11-09 19:12:23 +0100

    Make some suggestions in the code

--- src/Predavanje02/Inventory.kt
+++ src/Predavanje02/Inventory.kt
@@ -1,65 +1,80 @@
 package Predavanje02
 
+import kotlin.system.exitProcess
 
 
 fun main() {
     println("Inventory management system")
     val inventory = Inventory()
+    val loginHelper = LoginHelper()
     var exit = 0
     println("Login name:")
     val username = readln()
-    if (login(Employee.employees, username, inventory)) {
-        do {
-            println(inventory.currentUser.name)
-            println("Press number: 1. Add Employee, 2. Add Product, 3. Update Stock, 4. Print all products 5. Exit")
-            when (readln().toInt()) {
-                1 -> {
-                    println("Name: ")
-                    val name = readln()
-                    println("Last name: ")
-                    val lastName = readln()
-                    println("Add role: 1. Admin, 2. Manager, 3. Worker")
-                    val roleValue = readln().toInt()
-                    val role = Role.entries.find { it.value == roleValue }
-                    inventory.currentUser.addEmployee(Employee(name, lastName, role!!))
-                }
-                2 -> {
-                    println("Title: ")
-                    val title = readln()
-                    println("Category: 1. Food, 2. Tech, 3. Book")
-                    val category = readln().toInt()
-                    val productCategory = ProductCategory.entries.find { it.value == category }
-                    println("Quantity: ")
-                    val quantity = readln().toInt()
-                    inventory.addToInventory(Product(title, productCategory!!, quantity))
-                }
-                3 -> {
-                    println("Title: ")
-                    val title = readln()
-                    println("1. Increase product stock, 2. Decrease product stock")
-                    val action = readln().toInt()
-                    println("Quantity: ")
-                    val quantity = readln().toInt()
-                    when(action) {
-                        1 -> inventory.updateStock(title, quantity, StockUpdate.ADD)
-                        2 -> {
-                            inventory.updateStock(title, quantity, StockUpdate.REMOVE)
-                        }
-                    }
-
-                }
-                4 -> inventory.printAllProducts()
-                5 -> exit = 5
-                else -> {}
-            }
-        } while (exit != 5)
+    loginHelper.performLogin(
+        username = username,
+        onFailure = { exitProcess(0) }) { currentUser ->
+        // Proceed with the program. The current user is available here if needed.
     }
+    do {
+        println(inventory.currentUser.name)
+        println("Press number: 1. Add Employee, 2. Add Product, 3. Update Stock, 4. Print all products 5. Exit")
+        when (readln().toInt()) {
+            1 -> {
+                println("Name: ")
+                val name = readln()
+                println("Last name: ")
+                val lastName = readln()
+                println(buildString {
+                    Role.entries.forEachIndexed { index, role -> append("${index + 1}.$role ") }
+                })
+                val input = readln().toIntOrNull()
+                val roleIndex = if (input == null || (input - 1) >= Role.entries.count()) {
+                    println("Unsupported operation! Exiting app...")
+                    exitProcess(0)
+                } else {
+                    input - 1
+                }
+                val role = Role.entries[roleIndex]
+                inventory.currentUser.addEmployee(Employee(name, lastName, role))
+            }
 
+            2 -> {
+                println("Title: ")
+                val title = readln()
+                println("Category: 1. Food, 2. Tech, 3. Book")
+                val category = readln().toInt()
+                val productCategory = ProductCategory.entries.find { it.value == category }
+                println("Quantity: ")
+                val quantity = readln().toInt()
+                inventory.addToInventory(Product(title, productCategory!!, quantity))
+            }
+
+            3 -> {
+                println("Title: ")
+                val title = readln()
+                println("1. Increase product stock, 2. Decrease product stock")
+                val action = readln().toInt()
+                println("Quantity: ")
+                val quantity = readln().toInt()
+                when (action) {
+                    1 -> inventory.updateStock(title, quantity, StockUpdate.ADD)
+                    2 -> {
+                        inventory.updateStock(title, quantity, StockUpdate.REMOVE)
+                    }
+                }
+
+            }
+
+            4 -> inventory.printAllProducts()
+            5 -> exit = 5
+            else -> {}
+        }
+    } while (exit != 5)
 }
 
-fun login(employees: List<Employee>, name: String, inventory: Inventory) : Boolean {
+fun login(employees: List<Employee>, name: String, inventory: Inventory): Boolean {
     val employee = employees.indexOfFirst { it.name == name }
-    return if(employee != -1) {
+    return if (employee != -1) {
         inventory.setUser(employees[employee])
         true
     } else {
@@ -84,7 +99,7 @@ class Inventory() {
 
     fun updateStock(title: String, quantity: Int, method: StockUpdate) {
         val index = inventory.indexOfFirst { it.title == title }
-        if(method.equals(StockUpdate.ADD)) {
+        if (method.equals(StockUpdate.ADD)) {
             inventory[index].stock += quantity
         } else {
             inventory[index].stock -= quantity
@@ -92,10 +107,9 @@ class Inventory() {
     }
 
     fun printAllProducts() {
-        inventory.forEach{
-            println("${it.title} ${it.category} ${it.stock}" )
+        inventory.forEach {
+            println("${it.title} ${it.category} ${it.stock}")
         }
     }
-
 }
 
---
+++ src/Predavanje02/LoginHelper.kt
@@ -0,0 +1,14 @@
+package Predavanje02
+
+import Predavanje02.Employee.Companion.employees
+
+class LoginHelper {
+    fun performLogin(username: String, onFailure: () -> Unit, onLogin: (Employee) -> Unit) {
+        val employee = employees.find { it.name == username }
+        if (employee != null) {
+            onLogin(employee)
+        } else {
+            onFailure()
+        }
+    }
+}
