d9572458fa31a20797a3095604d1f0576a7f66d7
m2lovric <m2lovric@gmail.com>
2024-11-12 20:02:40 +0100

    Fix

--- src/Predavanje02/LoginHelper.kt
+++ src/Predavanje02/LoginHelper.kt
@@ -1,7 +1,5 @@
 package Predavanje02
 
-import Predavanje02.Employee.Companion.employees
-
 class LoginHelper {
     fun performLogin(username: String, onFailure: () -> Unit, onLogin: (Employee) -> Unit) {
         val employee = Admin.getEmployees().find { it.name == username }
